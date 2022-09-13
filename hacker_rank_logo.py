c32dd035f8086dcb7c15f62eeb16a12468ce0798
Mahesh_Srivinay <rmsvinay123@gmail.com>
2022-09-13 13:45:33 +0100

    Create hacker_rank_logo.py

---
+++ hacker_rank_logo.py
@@ -0,0 +1,23 @@
+#text alignment
+import math
+thickness = int(input())
+if thickness%2 !=0:
+    c = 'H'
+
+    for i in range(thickness):
+        print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness*5))
+        if i == thickness-1:
+            for j in range(thickness+1):
+                print((c*thickness).rjust(thickness + math.floor((thickness/2)))+(c*thickness).rjust((thickness*5)-thickness))
+                if j == thickness:
+                    for k in range(math.ceil(thickness/2)):
+                        print((c*(thickness*5)).rjust(thickness*5 + math.floor((thickness/2))))
+                        if k == (math.ceil(thickness/2)-1):
+                            for l in range(thickness+1):
+                                print((c*thickness).rjust(thickness + math.floor((thickness/2))) + (c*thickness).rjust((thickness*5)-thickness))
+                                if l == thickness:
+                                    for m in range(thickness):
+                                        print((c*(thickness-(m+1))).rjust((thickness*5)-1)+c+(c*(thickness-(m+1))).ljust((thickness*5)-(thickness)))
+                            
+else:
+    thickness = int(input())
